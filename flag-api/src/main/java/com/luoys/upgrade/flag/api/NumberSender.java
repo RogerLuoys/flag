1c7f2fe7b19fe811300c98b4b9c6a566c7624c32
luoys <luoys@servyou.com.cn>
2020-07-15 19:19:13 +0800

    update

--- flag-api/src/main/java/com/luoys/upgrade/flag/api/NumberSender.java
+++ flag-api/src/main/java/com/luoys/upgrade/flag/api/NumberSender.java
@@ -1,5 +1,17 @@
 package com.luoys.upgrade.flag.api;
 
+import java.util.Random;
+
 public class NumberSender {
-    long time1 = System.currentTimeMillis();
+    static Random rd = new Random();
+
+    public static String flagID() throws InterruptedException {
+
+        Thread.sleep(1);
+        long time1 = System.currentTimeMillis();
+        int num = rd.nextInt(9);
+        String id = "1" + Long.toString(time1) + Long.toString(num);
+        System.out.println(id);
+    }
+
 }
