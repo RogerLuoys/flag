d9276f1b2537f28416f714b6f2511be489b71867
luoys <luoys@guahao.com>
2020-12-11 22:02:01 +0800

    新增定时任务

---
+++ flag-schedule/src/main/java/com/luoys/upgrade/flag/schedule/TaskSchedule.java
@@ -0,0 +1,30 @@
+package com.luoys.upgrade.flag.schedule;
+
+import com.luoys.upgrade.flag.manage.TaskManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * @author luoys
+ * @since 2020-12-11
+ */
+@Component
+public class TaskSchedule {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TaskSchedule.class);
+
+    @Autowired
+    private TaskManager taskManager;
+
+    @Scheduled(cron = "0 0 8 * * ?")
+    public void automaticCreateWeekTask() {
+        LOG.info("=====定时任务:按周期生成每日任务开始 {}=====", new Date());
+
+        LOG.info("=====定时任务:按周期生成每日任务结束 {}=====", new Date());
+    }
+}
